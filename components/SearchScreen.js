44a94164360270d50c82ae1fd89dfb87bd031e9b
golden_arm4git config --global user.email softprince4799@gmail.comgit config --global user.name golden_arm4 <softprince4799@gmail.com>
2020-12-10 12:41:48 +0530

    new app

---
+++ components/SearchScreen.js
@@ -0,0 +1,140 @@
+import React, { useState,useEffect } from 'react';
+import { Text, View, StyleSheet, StatusBar, ImageBackground, Dimensions, } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Searchbar, Button } from 'react-native-paper';
+import Results from './results';
+import AsyncStorage from '@react-native-community/async-storage'
+// 
+const Search = () => {
+  var [url, setURL] = useState('loading...')
+  var [city, setCity] = useState('');
+  const [data, setData] = useState({
+    name: "loading...",
+    temp: "loading...",
+    humidity: "loading",
+    clouds: "loading...",
+    country: "loading",
+    icon: "-",
+  });
+  useEffect(()=>{
+    async function getData (){
+      url=(await AsyncStorage.getItem('image'))
+      city=(await AsyncStorage.getItem('city'))
+      fetcCities(city)
+    }
+    console.log(city +"from use effect");
+    console.log(url +"from use effect");
+  })
+  console.log(city);
+  
+ async function image (text) {
+    setURL(text);
+    (await AsyncStorage.setItem("image",url))
+    console.log(url + "from SearchScreen");
+  }
+
+
+  const fetcCities =async (text) => {
+    setCity(text)
+      
+      await AsyncStorage.setItem("city",text)
+      console.log(city);
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${text}&appid=7b686b5a40de14e0bd5e63deb86f5ddf`, {
+      method: "GET",
+      mode: "no-cors",
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': "*",
+        "Access-Control-Allow-Headers": "*",
+      },
+    }, 
+    // console.log("running...by the way ")
+    )
+      .then(item => item.json())
+      .then((json) => {setData({
+        name: json.name,
+        temp: json.main.temp,
+        humidity: json.main.humidity,
+        clouds: json.weather[0].description,
+        country: json.sys.country,
+        icon: json.weather[0].icon,
+      })
+    })
+      .catch(function (err) {
+        console.log(err + "search error")
+      }).finally(() => console.log(`data loading....`))
+     
+  }
+
+  return (
+    <ImageBackground source={{ uri: url }} style={styles.image}>
+      <StatusBar hidden={true} />
+      <LinearGradient colors={['rgba(123, 116, 91, 0.81)', 'rgba(11, 10, 4, 0.81)']} style={styles.gradient}>
+        <Text style={styles.title}>Mausam</Text>
+        <Text style={styles.superScript}>  v1.0</Text>
+      </LinearGradient>
+      <Searchbar
+        placeholder='Search City'
+        placeholderTextColor="grey"
+        color='white'
+        iconColor="white"
+        value={`${city}`}
+        onChangeText={text => fetcCities(text)}
+        style={styles.search}
+      />
+      <Results data={data} image={image} />
+    </ImageBackground >
+  );
+};
+const styles = StyleSheet.create({
+  gradient: {
+    alignItems: 'center',
+    height: 70,
+    justifyContent: 'center',
+    borderBottomRightRadius: 80,
+    borderBottomLeftRadius: 80,
+    borderRadius: 30,
+    margin: 5,
+    borderColor: '#d1d1eb',
+    borderWidth: 2,
+    flexDirection:'row'
+  },
+  title: {
+    color: '#b2bddb',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    fontSize: 24,
+    fontFamily: 'serif'
+  },
+  header: {
+    backgroundColor: 'hsla(16, 2%, 67%, 1)',
+  },
+  search: {
+    borderColor: '#fff',
+    borderWidth: 2,
+    borderRadius: 30,
+    margin: 5,
+    backgroundColor: 'hsla(16, 2%, 67%, 0.35)',
+    color: "white"
+  },
+  button: {
+    backgroundColor: '#217cca',
+    borderRadius: 30,
+    width: 120,
+    alignSelf: 'center',
+  },
+  image: {
+    height: Dimensions.get('window').height,
+    width: Dimensions.get("window").width,
+    resizeMode: "contain",
+  },
+  superScript:{
+    // lineHeight:37, 
+    fontSize:14,
+    color:"#fff",
+    top:5
+  }
+});
+export default Search;
---
+++ components/results.js
@@ -0,0 +1,76 @@
+import React,{useState,useEffect} from 'react';
+import { StyleSheet,Image,Dimensions,Text,ImageBackground ,View} from 'react-native';
+import { Card, Title, Paragraph } from 'react-native-paper';
+import { LinearGradient } from 'expo-linear-gradient';
+const customwidth =Dimensions.get("window").width;
+const customheight =Dimensions.get("window").height;
+const Results = ({ data ,image}) => {
+    const [imageURL,setImageURL]=useState('loading...');
+    useEffect(()=>{
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=${data.name}&client_id=9C4g1ogzzfA8s57vpwYqp3To9IqEIIugUsk8oNM9fmQ`,{
+        method:"GET",
+        mode:"no-cors",
+        headers:{
+            Accept: 'application/json',
+                "Access-Control-Allow-Headers":"*",
+                "Access-Control-Allow-Origin":"*",
+                "Access-Control-Allow-Methods":"*",
+                "Content-Type":"application/json",
+            }
+        })
+        .then(item=>item.json())
+        .then((json)=>setImageURL(json.results[1].urls.regular))
+        .catch(function(err){console.log(err+"catch error in results...")})
+        .finally(()=>console.log("finally in results."))
+        image(imageURL);
+    });
+    return (
+        
+            
+         <View style={{justifyContent:'center'}}> 
+           <Text style={{left:15,fontSize:25, top:20,color:"#020203",fontFamily:'monospace',fontWeight:'bold'}}  >{(data.name).toUpperCase()}</Text>
+           <Text style={{left:15,fontSize:18, top:20,color:"#020203",fontFamily:'monospace'}} >{data.country}</Text>
+           <View style={{height:60,width:60,borderRadius:30}}>
+            <Image 
+               style={{
+                   width:65,
+                   height:65,
+                   left:230,bottom:50,
+                   backgroundColor: 'rgba(250, 247, 240, 0.4)',
+                   borderColor:"white",
+                   borderWidth: 2,
+                   borderRadius:40
+               }}
+               source={{uri:`https://openweathermap.org/img/w/${data.icon}.png`}}
+               />
+              </View>
+              <View style={styles.content}>
+            <Card.Content>
+                <Title style={{color:"black",}}>Weather in city</Title>
+            <Text  style={{color:"black",fontSize:16}} >Avg. Temperature: {(data.temp - 273.15).toFixed(2)}{'\u00b0'}C{'\n'}</Text>
+                <Text  style={{color:"black",fontSize:16}} >Sky:     {(data.clouds).toUpperCase()}{'\n'}</Text>
+                <Text  style={{color:"black",fontSize:16}} >Humidity:   {(data.humidity)}{'\n'}</Text>
+            </Card.Content>
+           </View>
+       </View>
+       
+    );
+};
+const styles = StyleSheet.create({
+    card: {
+        top:0,
+        height:customheight,
+        width:customwidth,
+        alignSelf:'center',
+        resizeMode:"cover",
+    },content:{
+        backgroundColor:'rgba(244, 240, 225, 0.3)',
+        padding:5,
+        margin:15,
+        borderRadius:30,
+        top:80,
+        borderWidth:2,
+        borderColor:'#fff'
+    }
+})
+export default Results;
